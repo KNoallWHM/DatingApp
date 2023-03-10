feabd1796172d0cc4f902f62801aa42c55237d90
Kevin Bradley Noall <kevin.b.noall@gmail.com>
2023-03-10 01:10:09 -0700

    End of Section 4

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")] // GET /api/users
+    public class BaseApiController
+    {
+        
+    }
+}
